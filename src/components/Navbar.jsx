2f7327a490116c5040aa8bbf16ec3407ee56349f
Ayushi Yadav <ayushiyadav1258@gmail.com>
2024-07-15 21:53:43 +0530

    last commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,6 @@
 import logo from "../assets/ayushiLogo.jpeg";
-import { FaLinkedin } from "react-icons/fa";
-import { FaGithub } from "react-icons/fa";
-import { FaInstagram } from "react-icons/fa";
+import { FaLinkedin, FaGithub, FaInstagram } from "react-icons/fa";
+
 const Navbar = () => {
   return (
     <nav className="mb-20 flex items-center justify-between py-6">
@@ -9,9 +8,27 @@ const Navbar = () => {
         <img className="mx-2 w-40" src={logo} alt="logo" />
       </div>
       <div className="m-8 flex items-center justify-center gap-4 text-2xl">
-        <FaLinkedin />
-        <FaGithub />
-        <FaInstagram />
+        <a
+          href="https://www.linkedin.com/in/ayushi-yadav-0a6a30247/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaLinkedin />
+        </a>
+        <a
+          href="https://github.com/ayuGit11"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaGithub />
+        </a>
+        <a
+          href="https://www.instagram.com/ayushiyadav1258/?utm_source=qr&igsh=MWEybXlkcGg3cW53eQ%3D%3D"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaInstagram />
+        </a>
       </div>
     </nav>
   );
